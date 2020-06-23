7ed99056837df02ce7b965159cae0d9c33166803
Mateus Paixão <mateus.paixao@corebiz.com.br>
2020-06-23 17:22:15 -0300

    finish course

--- get-quote.py
+++ get-quote.py
@@ -3,14 +3,16 @@ import random
 def primary():
   # print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
+  f = open("quotes.txt", "r+") # r+ read and write mode
+
+  f.write('Same quote for edit programaticaly \n')
   quotes = f.readlines()
   f.close()
 
   last = len(quotes) - 1
   rnd = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd], sep="", end="") # remove a new line after print
 
 if __name__== "__main__":
   primary()
